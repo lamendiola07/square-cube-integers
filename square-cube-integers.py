e3b63826b22fe31d88270db3a2060b1c7327c9bd
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-04-26 00:15:47 +0800

    [P-4] Added #comments for basic information and instructions for the program

---
+++ square-cube-integers.py
@@ -0,0 +1,5 @@
+# Mendiola, Logie A. | BSCPE 1-5 | Object-Oriented Programming | Assignment #4: P-4
+
+# Write a method in python that will create two separate text files after reading the source text file named integers.txt that contains 20 integers
+# The first output file will be named double.txt containing the square of all even integers found in integers.txt
+# The second file will be named triple.txt containing the cube of all odd numbers found in the integers.txt.
